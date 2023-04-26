c2fc00e429613b157817f8edd615f286f7fb50ac
gotdori <kboo333@naver.com>
2023-04-26 23:22:47 +0900

    20230426_집

--- src/main/java/com/ds/project01/controller/UserController.java
+++ src/main/java/com/ds/project01/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
 	public String user_write(RedirectAttributes redi) {
 		redi.addAttribute("deptList", service.deptList());
 		redi.addAttribute("hobbyList", service.hobbyList());
-		return "redirect:http://localhost:8082/user/write";
+		return "BT_write";
 	}
 
 	@PostMapping("/user/save")
